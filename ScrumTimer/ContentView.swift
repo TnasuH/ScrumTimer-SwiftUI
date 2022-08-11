77004d36891f6ab43ff9c4ba6e849f8da0ffb24e
Tarik Nasuhoglu <tnasuh@gmail.com>
2022-08-11 18:06:22 +0300

    Initial Commit

---
+++ ScrumTimer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ScrumTimer
+//
+//  Created by Tarik Nasuhoglu on 11.08.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ScrumTimer/ScrumTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumTimerApp.swift
+//  ScrumTimer
+//
+//  Created by Tarik Nasuhoglu on 11.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct ScrumTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

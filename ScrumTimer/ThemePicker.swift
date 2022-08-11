235691007439ec42717841a3ec0e39af3d41de3a
Tarik Nasuhoglu <tnasuh@gmail.com>
2022-08-11 23:51:33 +0300

    ThemePicker developed.

---
+++ ScrumTimer/ThemePicker.swift
@@ -0,0 +1,27 @@
+//
+//  ThemePicker.swift
+//  ScrumTimer
+//
+//  Created by Tarik Nasuhoglu on 11.08.2022.
+//
+
+import SwiftUI
+
+struct ThemePicker: View {
+    @Binding var selection: Theme
+    
+    var body: some View {
+        Picker("Theme", selection: $selection) {
+            ForEach(Theme.allCases) { theme in
+                ThemeView(theme: theme)
+                    .tag(theme)
+            }
+        }
+    }
+}
+
+struct ThemePicker_Previews: PreviewProvider {
+    static var previews: some View {
+        ThemePicker(selection: .constant(.periwinkle))
+    }
+}

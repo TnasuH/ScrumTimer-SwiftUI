6cfe2ef2c0381945e35ea0f0d9063cea7cd463d7
Tarik Nasuhoglu <tnasuh@gmail.com>
2022-08-13 03:04:46 +0300

    Transcribing speech to text development completed.

--- ScrumTimer/Views/MeetingView.swift
+++ ScrumTimer/Views/MeetingView.swift
@@ -11,7 +11,9 @@ import AVFoundation
 struct MeetingView: View {
     @Binding var scrum: DailyScrum
     @StateObject var scrumTimer = ScrumTimer()
-    
+    @StateObject var speechRecognizer = SpeechRecognizer()
+    @State private var isRecording = false
+
     private var player: AVPlayer { AVPlayer.sharedDingPlayer }
 
     var body: some View {
@@ -20,7 +22,7 @@ struct MeetingView: View {
                             .fill(scrum.theme.mainColor)
             VStack {
                 MeetingHeaderView(secondsElapsed: scrumTimer.secondsElapsed, secondsRemaining: scrumTimer.secondsRemaining, theme: scrum.theme)
-                MeetingTimerView(speakers: scrumTimer.speakers, theme: scrum.theme)
+                MeetingTimerView(speakers: scrumTimer.speakers, theme: scrum.theme, isRecording: isRecording)
                 MeetingFooterView(speakers: scrumTimer.speakers, skipAction: scrumTimer.skipSpeaker)
             }
             
@@ -33,11 +35,17 @@ struct MeetingView: View {
                 player.seek(to: .zero)
                 player.play()
             }
+            speechRecognizer.reset()
+            speechRecognizer.transcribe()
+            isRecording = true
             scrumTimer.startScrum()
         }
         .onDisappear {
             scrumTimer.stopScrum()
-            let newHistory = History(attendees: scrum.attendees, lengthInMinutes: scrum.timer.secondsElapsed / 60)
+            speechRecognizer.stopTranscribing()
+            isRecording = false
+            let newHistory = History(attendees: scrum.attendees, lengthInMinutes: scrum.timer.secondsElapsed / 60, transcript: speechRecognizer.transcript)
+
             scrum.history.insert(newHistory, at: 0)
         }
         .navigationBarTitleDisplayMode(.inline)
